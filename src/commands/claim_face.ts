7bf6782fe9b4263fd2493dade429f7009aded0a1
ikec <tszmingjack@gmail.com>
2023-11-22 12:45:19 -0800

    claim face app

---
+++ src/commands/claim_face.ts
@@ -0,0 +1,42 @@
+import { Command } from '@sapphire/framework';
+import { ActionRowBuilder, ApplicationCommandType, EmbedBuilder, StringSelectMenuBuilder } from 'discord.js';
+import { GetData } from '../database';
+
+export class SlashCommand extends Command {
+    public constructor(context: Command.LoaderContext, options: Command.Options) {
+        super(context, {
+            ...options,
+            description: 'Delete message and ban author.'
+        });
+    }
+
+    public override registerApplicationCommands(registry: Command.Registry) {
+        registry.registerContextMenuCommand((builder) =>
+            builder //
+                .setName(this.name)
+                .setType(ApplicationCommandType.Message)
+        );
+    }
+
+    public override async contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
+        await interaction.deferReply();
+        if (interaction.isMessageContextMenuCommand()) {
+            const uid = await GetData('User', interaction.user.id);
+            if (uid == undefined) return interaction.editReply('You do not have characters.');
+
+            const chars = uid['characters'];
+            return interaction.followUp({
+                components: [
+                    new ActionRowBuilder<StringSelectMenuBuilder>()
+                        .addComponents(
+                            new StringSelectMenuBuilder()
+                                .setCustomId(`claim-face-menu_${interaction.targetId}`)
+                                .setPlaceholder('Select a reason')
+                                .addOptions(chars.map((char: any) => { return { label: char['fullname'], value: char['fullname'] } }))
+                        )
+                ],
+                embeds: [ new EmbedBuilder().setTitle("Claim this face for your character:") ]
+            });
+        }
+    }
+}
---
+++ src/interaction-handlers/claim-face/menu.ts
@@ -0,0 +1,72 @@
+import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
+import { EmbedBuilder, ForumChannel, StringSelectMenuInteraction, ThreadChannel } from 'discord.js';
+import bot from '../../bot';
+import { GetData, SaveData } from '../../database';
+
+export class InventorySelectHandler extends InteractionHandler {
+    public constructor(ctx: InteractionHandler.LoaderContext, options: InteractionHandler.Options) {
+        super(ctx, {
+            ...options,
+            interactionHandlerType: InteractionHandlerTypes.SelectMenu
+        });
+    }
+
+    public override async parse(interaction: StringSelectMenuInteraction) {
+        if (!interaction.customId.startsWith('claim-face-menu')) return this.none();
+        const id = interaction.customId.split('_')[1];
+        return this.some(id);
+    }
+
+    public override async run(interaction: StringSelectMenuInteraction, received: string) {
+        await interaction.deferReply();
+
+        const requested_message = await interaction.channel?.messages.fetch(received);
+        if (requested_message == undefined) return interaction.editReply('Requested message not found.');
+
+        const requested_image = requested_message.attachments.first();
+        if (requested_image == undefined) return interaction.editReply('Requested image not found.');
+
+        const selected = interaction.values[0];
+        const uid = await GetData('User', interaction.user.id);
+        if (uid == undefined) return interaction.editReply('You do not have characters.');
+
+        const chars = uid['characters'];
+        const char = chars.find((char: any) => char['fullname'] == selected);
+
+        const str = char['thread'].split('/');
+        const thread = await bot.channels.fetch(str[str.length - 1]).catch((e) => {
+            console.log(e);
+            return undefined;
+        }) as ThreadChannel;
+        if (thread == undefined) return interaction.editReply('Character thread not found.');
+
+        const messages = await thread.messages.fetch();
+        if (messages == undefined) return interaction.editReply('Messages not found.');
+
+        const target_message = messages.last()!;
+        const target_embed = target_message.embeds[0]!;
+        const new_embed = new EmbedBuilder(target_embed.toJSON());
+        new_embed.setImage(requested_image.url);
+
+        const forum = (thread.parent as ForumChannel);
+        const tag = forum.availableTags.find(t => t.name === interaction.user.username);
+        if (tag == undefined) return interaction.editReply('Tag not found.');
+
+        await thread.delete();
+        const new_thread = await forum.threads.create({
+            name: char['fullname'],
+            message: {
+                content: `${interaction.user}`,
+                embeds: [new_embed],
+            },
+            appliedTags: [tag.id],
+        });
+
+        char['thread'] = new_thread.url;
+        const i = chars.findIndex((char: any) => char['fullname'] == selected);
+        uid['characters'][i] = char;
+        await SaveData('User', interaction.user.id, uid)
+
+        return await interaction.deleteReply();
+    }
+}
