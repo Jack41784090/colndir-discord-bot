4ade0f4f1426aef0e3f3a262f95a4688774da79e
ikec <tszmingjack@gmail.com>
2023-11-22 15:47:40 -0800

    structural change and refresh command

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -4,3 +4,19 @@ export function capitalize(string: string): string {
 export function formalise(string: string): string {
     return string.split(/[\s_-]+/).map(s => capitalize(s.toLowerCase())).join(" ");
 }
+
+export function empty_ud(): Record<string, any> {
+    return {
+        characters: [],
+    };
+}
+
+
+export function character(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
