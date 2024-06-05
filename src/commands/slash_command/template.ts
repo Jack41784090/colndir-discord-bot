51dedceaff9286d90dde80e9567963768a2e0c0c
ikec <tszmingjack@gmail.com>
2024-06-05 08:52:58 -0700

    feat: Add new slash command template

---
+++ src/commands/slash_command/template.ts
@@ -0,0 +1,27 @@
+import { ChatInputCommand, Command } from '@sapphire/framework';
+import { PermissionFlagsBits } from 'discord.js';
+
+// [COMMAND NAME]: [DESCRIPTION]
+export class TESTCommand extends Command {
+    public constructor(context: Command.LoaderContext, options: Command.Options) {
+        super(context, {
+            ...options,
+            description: 'This is the normal description',
+            detailedDescription: 'This is the detailed description',
+            requiredUserPermissions: [PermissionFlagsBits.Administrator]
+        });
+    }
+
+    public override async registerApplicationCommands(registry: ChatInputCommand.Registry) {
+        registry.registerChatInputCommand((builder) =>
+            builder
+                .setName('test')
+                .setDescription('Ping bot to see if it is alive')
+        );
+    }
+
+    public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+        await interaction.deferReply();
+        await interaction.followUp('Pong!');
+    }
+}
