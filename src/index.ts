a74f6ff36f72d69ec3ac28a4288f696708f65d15
ikec <tszmingjack@gmail.com>
2024-10-31 16:06:45 -0700

    👷Adding https to open up a port for CloudRun

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+import bot from '@bot';
 import { ApplicationCommandRegistries, RegisterBehavior } from "@sapphire/framework";
-import bot from "./bot";
+import * as http from 'http';
+
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
 bot.login(process.env.TOKEN)
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Just for testing purposes\n');
+});
+    
+server.listen(process.env.CLOUDRUN_PORT, () => {
+    console.log('Hello world listening on port', process.env.CLOUDRUN_PORT);
+});
