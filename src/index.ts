e2a12914d2037d0e94c73c02b0f28f26901334a5
ikec <tszmingjack@gmail.com>
2024-11-05 14:36:42 -0800

    :bug: testing with bot.login initialisation logs
    👷 Deploying to CloudRun

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import bot from '@bot';
 import { ApplicationCommandRegistries, RegisterBehavior } from "@sapphire/framework";
 import * as http from 'http';
 
+// require('dotenv').config();
+
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
-bot.login(process.env.TOKEN)
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -11,5 +12,14 @@ const server = http.createServer((req, res) => {
 });
     
 server.listen(process.env.CLOUDRUN_PORT, () => {
-    console.log('Hello world listening on port', process.env.CLOUDRUN_PORT);
+    console.log('Server running at http://localhost:' + process.env.CLOUDRUN_PORT + '/');
+    console.log(`bot.id: ${bot.id}`)
+    console.log(`TOKEN: ${process.env.TOKEN}`)
+    bot.login(process.env.TOKEN)
+        .then(() => {
+            console.log('Bot is ready');
+        })
+        .catch((error) => {
+            console.error('Error while logging in', error);
+        });
 });
